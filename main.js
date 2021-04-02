3330d9b467b916199c9f13226ee11946b1515b11
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-04-02 20:27:45 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var div = document.getElementById("contain")
+
+
+fetch('https://api.coinlore.net/api/coin/markets/?id=90')
+.then(response => response.json())
+.then(quote =>console.log(quote.map(element => {
+  for (const ele in element) {
+  var a= `<div class="q1">
+ <h1>${element.name}</h1>
+ <div class="pic"><img src="img/bitcoin_PNG27.png" width="100px" height="100px">  </div>
+ <div class="details">
+ <h2>Base : <span>${element.base}</span></h2>
+ <h2>Quote : <span>${element.quote}</span></h2>
+ <h2>Rupees : <span>${element.price}</span></h2>
+ <h2>USD : $<span>${element.price_usd}</span></h2>
+ <h2> Time :<span>${element.time}</span></h2>
+</div>`
+  }
+  div.insertAdjacentHTML('beforeend', a);
+})))
